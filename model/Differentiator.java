1f8e4811a190952f21ca877f29e06bebfc325cfc
Jacob <122387152+jacobklymenko@users.noreply.github.com>
2024-08-01 15:39:50 -0700

    added converting trees to math str expr

--- model/Differentiator.java
+++ model/Differentiator.java
@@ -1,5 +1,5 @@
 /**
- * ExpressionParser - Derivates Calculator
+ * ExpressionParser - Derivatives Calculator
  */
 
 package model;
@@ -7,8 +7,7 @@ package model;
 import structures.BinaryTreeNode;
 
 /**
- * Differentiator takes a binary tree representing a symbolic mathematical expression and
- * differentiates the expression.
+ * Differentiator differentiates a binary tree representing a symbolic mathematical expression.
  *
  * @author Jacob Klymenko
  * @version 1.0
@@ -21,56 +20,78 @@ public class Differentiator {
 	}
 
 	/**
+	 * Returns a String representing the derivative of the binary tree node root.
 	 *
-	 *
-	 * @param theRoot	the root of the binary tree representing the expression being derived
-	 * @param theVar	the chosen independent variable for differentiation
-	 * @return
+	 * @param theRoot		the root node representing the expression segment being derived
+	 * @param theVariable	the chosen independent variable for differentiation
+	 * @return a String representing the derivative of the binary tree node root
 	 */
 	public static String derive(final BinaryTreeNode<String> theRoot,
-	    final String theVar) {
-		String result = "";
-		if (theRoot != null) {
+	    final Character theVariable) {
+		// System.out.println("\n" + treeToString(theRoot));
+		String derivative = "";
+		if (theRoot != null) { // first base case here
 			final String rootElement = theRoot.getElement();
 			if (isOperator(rootElement)) {
-				final String left = theRoot.getLeft().getElement();
-				final String difLeft = derive(theRoot.getLeft(), theVar);
-				final String right = theRoot.getRight().getElement();
-				final String difRight = derive(theRoot.getRight(), theVar);
-				switch (rootElement) {
-					case "-":
-						result = difLeft + " " + rootElement + " " + difRight;
-						break;
-					case "+":
-						result = difLeft + " " + rootElement + " " + difRight;
-						break;
-					case "/":
-						result = "((" + difLeft + ")(" + right + ") - (" + difRight + ")(" +
-						    left + ")) / (" + right + ") ^ 2";
-						break;
-					case "*":
-						result = "(" + difLeft + ")(" + right + ") + (" + left + ")(" +
-						    difRight + ")";
-						break;
-					case "^":
-						result = right + left + " ^ (" + right + " - 1)";
-						break;
-				}
+				final String left = treeToString(theRoot.getLeft());
+				final String right = treeToString(theRoot.getRight());
+				// differentiated left subtree
+				final String diffLeft = derive(theRoot.getLeft(), theVariable);
+				// differentiated right subtree
+				final String diffRight = derive(theRoot.getRight(), theVariable);
+				derivative = deriveOperator(left, right, diffLeft, diffRight, rootElement);
 			} else if (ExpressionParser.isFunction(rootElement)) {
 
-			} else { // a constant or variable
-				if (!rootElement.matches(".*[a-zA-Z].*")) { // a constant
-					result = "0";
-				} else { // a variable
-					if (rootElement.matches("-?\\d+(\\.\\d+)?")) { // constant * variable
-						result = rootElement.replaceAll("[^\\d.]", ""); // removes all letters
-					} else {
-						result = "1";
+			} else { // second base case here - a constant or variable
+				if (rootElement.matches(".*" + theVariable + ".*")) { // contains a variable
+					if (rootElement.length() > 1) { // constant * variable of differentiation
+						char ch = Character.MIN_VALUE;
+						derivative = rootElement.replace(theVariable, ch);
+					} else { // the variable of differentiation is alone
+						derivative = "1";
 					}
+				} else { // only contains a constant
+					derivative = "0";
 				}
 			}
 		}
-		return result;
+		return derivative;
+	}
+
+	/**
+	 * Applies the corresponding operator differentiation rule and returns a String
+	 * representing differentiated expression.
+	 *
+	 * @param theLeft 		a String representing the left subtree of the expression
+	 * @param theRight		a String representing the right subtree of the expression
+	 * @param theDiffLeft	a String representing the differentiated left subtree
+	 * @param theDiffRight 	a String representing the differentiated left subtree
+	 * @param theOperator	the operator determining the differentiation rule to apply
+	 * @return a String representing differentiated expression
+	 */
+	private static String deriveOperator(final String theLeft, final String theRight,
+	    final String theDiffLeft, final String theDiffRight, final String theOperator) {
+		String derivative = "";
+		switch (theOperator) {
+			case "-":
+				derivative = theDiffLeft + " " + theOperator + " " + theDiffRight;
+				break;
+			case "+":
+				derivative = theDiffLeft + " " + theOperator + " " + theDiffRight;
+				break;
+			case "/":
+				derivative = "((" + theDiffLeft + ")(" + theRight + ") - (" + theDiffRight +
+				    ")(" + theLeft + ")) / (" + theRight + ") ^ 2";
+				break;
+			case "*":
+				derivative = "(" + theDiffLeft + ")(" + theRight + ") + (" + theLeft + ")(" +
+				    theDiffRight + ")";
+				break;
+			case "^": // ERROR - implementing handling diff var in the exponent
+				derivative = theRight + theLeft + " ^ (" + theRight + " - 1)";
+				break;
+		}
+		return derivative;
 	}
 
 	/**
@@ -88,8 +109,34 @@ public class Differentiator {
 		return result;
 	}
 
-	// public static String treeToString(final BinaryTree<String> theOutputTree) {
-	// // TODO
-	// return null;
-	// }
+	/**
+	 * Returns a String representing the binary tree as a mathematical expression recursively.
+	 *
+	 * @param theRoot the root node of this binary tree
+	 * @return a String representing the binary tree as an expression
+	 */
+	private static String treeToString(final BinaryTreeNode<String> theRoot) {
+		String result = "";
+		if (isOperator(theRoot.getElement())) {
+			/*
+			 * Improves final expression readability by adding parentheses surrounding chunks
+			 * of the tree. However, the bigger the tree, the messier the String becomes. Best
+			 * thing to do in the future is to use apply parentheses around two operands and
+			 * an operator. The operands could be other chunks of expressions, like "(x ^ 2)".
+			 *
+			 * Operator Example: "(<operand> <operator> <operand>)"
+			 * Function Example: "(<function>(<operand>))"
+			 */
+			if (theRoot.numChildren() == 2 || theRoot.numChildren() == 6) {
+				// result = "(" + treeToString(theRoot.getLeft()) + " " +
+				// theRoot.getElement() + " " + treeToString(theRoot.getRight()) + ")";
+				// } else {
+				result = treeToString(theRoot.getLeft()) + " " +
+				    theRoot.getElement() + " " + treeToString(theRoot.getRight());
+			}
+		} else { // the base case is when the root is a constant or variable
+			result = theRoot.getElement();
+		}
+		return result;
+	}
 }
